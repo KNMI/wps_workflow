235224b2c710dca11f27c1f071813b35028f8b12
andrejsim <andrej@knmi.nl>
2017-01-04 15:03:12 +0100

    preparing demo.

---
+++ run.workflow.sh
@@ -0,0 +1,5 @@
+
+#python -m dispel4py.new.processor simple clipc -f input_simple.clipc
+python -m dispel4py.new.processor multi clipc -n 12 -f input_simple.json
+#python -m dispel4py.new.processor multi clipc -n 12 -f input_simple.clipc
+
