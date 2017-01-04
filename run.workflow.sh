f2028c41f92b85e9935e701beaff52157f998764
andrejsim <andrej@knmi.nl>
2017-01-04 16:12:04 +0100

    starting to create simple example documents.

--- run.workflow.sh
+++ run.workflow.sh
@@ -1,5 +1,6 @@
 
-#python -m dispel4py.new.processor simple clipc -f input_simple.clipc
-python -m dispel4py.new.processor multi clipc -n 12 -f input_simple.json
-#python -m dispel4py.new.processor multi clipc -n 12 -f input_simple.clipc
+# run in jupyter?
+python -m dispel4py.new.processor simple clipc -f input_simple.json
 
+# run outside of jupyter for multiprocessing.
+#python -m dispel4py.new.processor multi clipc -n 12 -f input_simple.json
